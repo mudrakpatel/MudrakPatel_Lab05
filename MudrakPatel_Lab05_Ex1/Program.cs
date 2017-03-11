8a7283be14ab434e671115c60401a19e54dbca90
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-11 18:28:13 -0500

    Still stuck on Ex1

--- MudrakPatel_Lab05_Ex1/Program.cs
+++ MudrakPatel_Lab05_Ex1/Program.cs
@@ -62,11 +62,15 @@ namespace MudrakPatel_Lab05_Ex1
             #endregion
             /////////////////////////////
             var doubleList = new CustomList();
-            for (int index = 0; index < 12; index++)
-            {
-                doubleList.InsertAtBack(index);
-            }
-
+            var node1 = new CustomListNode(1.0, null);
+            //var node2 = new CustomListNode(2.0, node1);
+            //var node3 = new CustomListNode(3.0, node2);
+            //var node4 = new CustomListNode(4.0, node3);
+            //var node5 = new CustomListNode(5.0, node4);
+            //var node6 = new CustomListNode(6.0, node1);
+            Console.WriteLine("\n>>> Display method called...\n"); //Display method
+            doubleList.Display();
+            Console.WriteLine("\n >>> The count of the list is: {0,2}\n", CustomList.Count(doubleList));
         }
     }
 }
