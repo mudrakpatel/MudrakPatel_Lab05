0656c0583be8fb3de9298edb36544a3f9cb42758
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-10 19:49:24 -0500

    Added List variable named "doubleList"

--- MudrakPatel_Lab05_Ex1/Program.cs
+++ MudrakPatel_Lab05_Ex1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Import the custom LinkedListLibrary_Mudrak
+using LinkedListLibrary_Mudrak;
 
 namespace MudrakPatel_Lab05_Ex1
 {
@@ -10,6 +12,8 @@ namespace MudrakPatel_Lab05_Ex1
     {
         static void Main(string[] args)
         {
+            List doubleList = new List("doubleList");
+            
         }
     }
 }
