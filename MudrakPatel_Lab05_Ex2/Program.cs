5b7665f8ec459d624daae13f3102ed0f77ff99e2
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-12 06:16:57 -0400

    Ex2 Push method working

--- MudrakPatel_Lab05_Ex2/Program.cs
+++ MudrakPatel_Lab05_Ex2/Program.cs
@@ -12,6 +12,16 @@ namespace MudrakPatel_Lab05_Ex2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n>>> Creating Stack...\n");
+            var stackList = new StackInheritance();
+            Console.WriteLine("\n>>> Populating Stack with some data...\n");
+            for (int index = 0; index < 5; index++)
+            {
+                stackList.Push(index); //Call Push method
+            }
+            Console.WriteLine("\n>>> Called Display method...\n");
+            stackList.Display();
+            Console.WriteLine("\n>>> Called Pop method...\n");
 
         }
     }
