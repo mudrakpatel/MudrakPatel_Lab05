f3b97fda5450e8d87bd0d2652d9aad22e703cc69
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-12 07:07:53 -0400

    Ex2 complete

--- MudrakPatel_Lab05_Ex2/Program.cs
+++ MudrakPatel_Lab05_Ex2/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 //Import StackInheritenceLibrary
 using StackInheritanceLibrary;
+using LinkedListLibrary;
 
 namespace MudrakPatel_Lab05_Ex2
 {
@@ -12,17 +13,33 @@ namespace MudrakPatel_Lab05_Ex2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n>>> Creating Stack...\n");
-            var stackList = new StackInheritance();
-            Console.WriteLine("\n>>> Populating Stack with some data...\n");
-            for (int index = 0; index < 5; index++)
+            try
             {
-                stackList.Push(index); //Call Push method
+                Console.WriteLine("\n>>> Creating Stack...\n");
+                var stackList = new StackInheritance();
+                Console.WriteLine("\n>>> Populating Stack with some data...\n");
+                for (int index = 0; index < 5; index++)
+                {
+                    stackList.Push(index); //Call Push method
+                }
+                Console.WriteLine("\n>>> Called Display method...\n");
+                stackList.Display();
+                Console.WriteLine("\n>>> Called Pop method...\n");
+                Console.WriteLine("\n--- Popped element: {0,2}\n", stackList.Pop()); //Call Pop method
+                Console.Write("\n--- Stack data: ");
+                stackList.Display();
+                Console.WriteLine(); //Blank line
+                Console.WriteLine("\n>>> Called Peek method...\n");
+                Console.WriteLine("\n--- Peeked element: {0,2}\n", stackList.Peek()); //Call Peek method
             }
-            Console.WriteLine("\n>>> Called Display method...\n");
-            stackList.Display();
-            Console.WriteLine("\n>>> Called Pop method...\n");
-            Console.WriteLine("\n--- Popped element: {0,2}\n", stackList.Pop());
+            catch (EmptyListException exception)
+            {
+                Console.WriteLine("\n>>> Exception message: {0,2}\n", exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("\n>>> Exception message: {0,2}\n", exception.Message);
+            }
         }
     }
 }
