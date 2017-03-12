7e26f25ab51350b232a6244cc93e034ae8bf43fe
mudrakpatel <mudrakpatel96@gmail.com>
2017-03-12 05:22:37 -0400

    Added Ex2

---
+++ MudrakPatel_Lab05_Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudrakPatel_Lab05_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
